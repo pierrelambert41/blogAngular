fd83459b33fd5a010b53ce1f782257c7794c6294
plambert <pierre.lambert@mind7.com>
2018-11-21 11:01:52 +0100

    add final solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-blog';
+  
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Puto carnifex sollemniter et ut Aegypto fas et cum iudiciis et societate per causarum cladium recensere discernente cladium internarum tempus fidem velut internarum Parthica',
+      loveIts: 1,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxieme post',
+      content: 'Puto carnifex sollemniter et ut Aegypto fas et cum iudiciis et societate per causarum cladium recensere discernente cladium internarum tempus fidem velut internarum Parthica',
+      loveIts: -1,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon troisieme post',
+      content: 'Puto carnifex sollemniter et ut Aegypto fas et cum iudiciis et societate per causarum cladium recensere discernente cladium internarum tempus fidem velut internarum Parthica',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
